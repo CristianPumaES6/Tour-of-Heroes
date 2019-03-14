aef40661a740f8d46f6d6fe8bba257d2aaf830e4
CristianPumaES6 <cristian.puma.es6@gmail.com>
2019-03-13 19:05:09 -0500

    Subscribe in HeroesComponent + Observable HeroService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -15,16 +17,24 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService
+  ) { }
 
   ngOnInit() {
-    this.heroes = HEROES;
+    this.getHeroes();
   }
 
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     console.log(this.selectedHero)
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(
+      heroes => this.heroes = heroes
+    );
   }
 
 }
